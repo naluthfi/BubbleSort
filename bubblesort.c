553177a56380d6f3fbb6ee3683c3bf0d11889e5d
luthfi <naluthfi@gmail.com>
2018-06-13 14:24:31 +0700

    add execution time

--- bubblesort.c
+++ bubblesort.c
@@ -1,49 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main() {
+int main() 
+{
 	int total, step=1;
-	printf("\nInsert total number you want to sort(example: 5): ");
-	scanf("%d" , &total);
-	printf("\nInsert all numbers you want to sort: ");
+	printf ("\nInsert total number you want to sort(example: 5): ");
+	scanf ("%d" , &total);
+	printf ("\nInsert all numbers you want to sort: ");
 	int num[total];
-	for (int i=0;i<total;i++){
-		scanf("%d" , &num[i]);
-	}
+	for (int i = 0; i < total; i++) 
+		scanf ("%d" , &num[i]);
 	/*
 	printf("This is your numbers: " );
-	for (int i=0;i<total;i++){
-		printf("%d " , num[i]);
-		if (i==total-1) printf("\n" );
+	for (int i = 0; i < total; i++){
+		printf ("%d " , num[i]);
+		if (i == total - 1) printf ("\n" );
 	}
 	*/
-	int temptotal=total;
-	for(int a=0;a<total;a++){
-		for(int i=0;i<temptotal-1;i++){
-			for (int j=i+1;j<temptotal;j++){
-				if(num[j] < num[i]){
-					int temp=num[i];
-					num[i]=num[j];
-					num[j]=temp;
-				}
-				/*
-				printf("\nstep %d = ", step);
-				step++;
-				for (int i=0;i<total;i++){
-					printf("%d " , num[i]);
-					if (i==total-1) printf("\n" );
-				}
-				*/
-				i++;
+	int temptotal = total;
+	int loop = 1;
+	struct timespec tstart={0,0}, tend={0,0};
+	clock_gettime(CLOCK_MONOTONIC, &tstart);
+	for (int a = 0; a < total; a++)
+	{
+		int i = 0;
+		printf ("\n\nloop %d", loop);
+		for (int j = (i + 1); j <= temptotal; j++)
+		{
+			if (num[j] < num[i])
+			{
+				int temp = num[i];
+				num[i] = num[j];
+				num[j] = temp;
 			}
-			i=0;
-			temptotal--;
-		}	
+			printf ("\nstep %d = ", step);
+			step++;
+			for (int i = 0; i < total; i++)
+				printf("%d " , num[i]);
+			i++;
+		}
+		temptotal--;
+		loop++;
 	}
-	
-	printf("\nAfter bubble sorting: " );
-	for (int i=0;i<total;i++){
-		printf("%d " , num[i]);
-		if (i==total-1) printf("\n" );
+	clock_gettime(CLOCK_MONOTONIC, &tend);
+	printf("\n\nExecution Time: %.9f seconds",
+        	((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) - 
+        	((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec));
+	printf ("\n\nAfter bubble sorting: ");
+	for (int i = 0; i < total; i++)
+	{
+		printf ("%d " , num[i]);
+		if (i == total - 1) printf ("\n");
 	}
 }
